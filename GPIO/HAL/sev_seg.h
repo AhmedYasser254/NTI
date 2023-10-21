673012d3bf6131c545db22064e8c36b7ba4f0cd9
Ahmed Yasser <106586965+AhmedYasser254@users.noreply.github.com>
2023-10-21 14:15:20 +0300

    After 7 SEG

---
+++ GPIO/HAL/sev_seg.h
@@ -0,0 +1,15 @@
+/*
+ * sev_seg.h
+ *
+ *  Created on: Oct 21, 2023
+ *      Author: Ahmed Yasser
+ */
+
+#ifndef SEV_SEG_H_
+#define SEV_SEG_H_
+
+
+#include "dio.h"
+void Segment_init(void);
+void SevenSeg_Num(uint8 num);
+#endif /* SEV_SEG_H_ */
