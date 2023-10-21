13128b3ede3f38b25876ed98b6646843bce93bda
Ahmed Yasser <106586965+AhmedYasser254@users.noreply.github.com>
2023-10-21 14:14:31 +0300

    After  7 SEG

--- GPIO/APP/app.c
+++ GPIO/APP/app.c
@@ -6,21 +6,40 @@
  */
 
 
-#include "gpio.h"
-DIOReg_Typedef* GPIOA = ((DIOReg_Typedef*)0x39);
-DIOReg_Typedef* GPIOB  = ((DIOReg_Typedef*)0x16);
+
+#include "sev_seg.h"
+#include <util/delay.h>
+//DIOReg_Typedef* GPIOA = ((DIOReg_Typedef*)0x39);
+//DIOReg_Typedef* GPIOB  = ((DIOReg_Typedef*)0x16);
+//
+
+//int main(void)
+//{
+//	GPIO_PinCOnffig PinCfg;
+//		PinCfg.dir = PIN_OUTPUT;
+//		PinCfg.pin = PIN_5;
+//	//	DIO_voidSetPinDirection(GPIOA,&PinCfg);
+//DIO_voidSetPortDirection(GPIOA, &PinCfg);
+//	while(1)
+//	{
+//		//DIO_setPinValue(GPIOA, &PinCfg, LOGIC_HIGH);
+//		DIO_voidSetPortValue(GPIOA, LOGIC_HIGH);
+//	}
+//return 0;
+//}
 
 
 int main(void)
 {
-	GPIO_PinCOnffig PinCfg;
-		PinCfg.dir = PIN_OUTPUT;
-		PinCfg.pin = PIN_5;
-		DIO_voidSetPinDirection(GPIOA,&PinCfg);
+	Segment_init();
 
 	while(1)
 	{
-		DIO_setPinValue(GPIOA, &PinCfg, LOGIC_HIGH);
+		for(int i=0; i<=9; i++)
+		{
+			 SevenSeg_Num(i);
+					 _delay_ms(1000);
+		}
 	}
 return 0;
 }
